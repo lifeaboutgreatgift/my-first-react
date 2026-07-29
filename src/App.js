056f4bedce3ec49f441844f6945e9cf6865b1439
Afroj <overthinker.ltd03@gmail.com>
2026-07-30 01:17:51 +0530

    feat: import and place btn in jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import subbtn from './assets/sub-btn.png'
+import restbtn from './assets/rest-btn.png'
+import addbtn from './assets/add-btn.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,9 +15,26 @@ function App() {
       <h1>My Counter</h1>
       <p className="count" style={{ color: color }}>{count}</p>
       <div className="buttons">
-        <button onClick={() => setCount(count - step)}>-</button>
-        <button onClick={() => setCount(0)}>Reset</button>
-        <button onClick={() => count < 10 && setCount(count + step)}>+</button>
+
+        
+        <button
+         onClick={() => setCount(count - step)}
+         className="ink-nav-button"
+         style={{ backgroundImage: `url(${subbtn})`}}
+         >
+         </button>
+        <button
+         onClick={() => setCount(0)}
+         className="ink-nav-button"
+         style={{ backgroundImage: `url(${restbtn})`}}
+         >
+         </button>
+        <button 
+        onClick={() => count < 10 && setCount(count + step)}
+        className="ink-nav-button"
+        style={{ backgroundImage: `url(${addbtn})`}}
+        >
+        </button>
       </div>
     </div>
   )
